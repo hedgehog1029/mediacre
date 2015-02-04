501d577b82341b0de683188d3f378474f3492c0d
hedgehog1029 <omgredstone@gmail.com>
2015-02-04 21:47:00 +0000

    Changing the purpose of Mediocre.

--- mediocre.js
+++ mediocre.js
@@ -1,10 +1,34 @@
-var speaker = require('speaker'),
-stream = require('./stream.js');
+/*
+* The Mediocre media server
+*/
 
-var speaker = new Speaker({
-	channels: 2,
-	bitDepth: 16,
-	sampleRate: 44100
+var lame = require("lame");
+
+var encoder = lame.Encoder({ channels: 2, bitDepth: 16, sampleRate: 44100 });
+encoder.on("data", function(data) {
+    sendData(data);
 });
 
-stream.playStream("http://rainwave.cc/tune_in/5.mp3", speaker);
+var decoder = lame.Decoder();
+decoder.on("format", function(format) {
+    decoder.pipe(encoder);
+});
+
+var clients = [];
+
+function sendData(data) {
+    clients.forEach(function(client) {
+        client.write(data);
+    });
+}
+
+var server = http.createServer(function(req, res){
+    res.writeHead(200,{
+        "Content-Type": "audio/mpeg",
+        'Transfer-Encoding': 'chunked'
+    });
+    // Add the response to the clients array to receive streaming
+    clients.push(res);
+    console.log('Client connected; streaming');
+});
+server.listen("8000", "127.0.0.1");
