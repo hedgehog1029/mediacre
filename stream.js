e7dfb1ae71ee1c805f67d56743492126f94e8cfb
hedgehog1029 <omgredstone@gmail.com>
2015-02-05 17:16:49 +0000

    Removed local reference to module.exports

--- stream.js
+++ stream.js
@@ -1,7 +1,5 @@
 var icecast = require('icecast');
 
-var exports = module.exports;
-
 exports.playStream = function(url, speaker) {
 	icecast.get(url, function(res) {
 		res.on('data', function(data) {
