b34baa59fa499196900c33fb524856aa6f0260b0
hedgehog1029 <omgredstone@gmail.com>
2015-02-05 21:18:02 +0000

    Spotify updates! Working towards web interface.

--- spotify.js
+++ spotify.js
@@ -1,9 +1,14 @@
 var cred = require('./passwd');
-var sp = require('libspotify');
+var sp = require('libspotify'),
+    colors = require("colors");
 
 var session = new sp.Session({
     applicationKey: './spotify_appkey.key'
 });
+
+var log = function(message) {
+    console.log("spotify > ".red + message);
+}
 
 session.login(cred.login, cred.password);
 session.once("login", function() {
@@ -19,4 +24,17 @@ exports.test = function(callback) {
         var player = session.getPlayer();
         callback(track, player);
     });
+}
+
+exports.search = function(search, trackCount, callback) {
+    var search = new sp.Search(search);
+    search.trackCount = trackCount;
+    search.execute();
+    search.once("ready", function() {
+        callback(search);
+    });
+}
+
+exports.getPlayer = function() {
+    return session.getPlayer();
 }
